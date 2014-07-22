d033393adbd0ee9c9a0a87aa4a9d7b094f6ec2dc
Jakub Cieslar <cieslar.jakub@gmail.com>
2014-07-22 21:28:33 +0200

    Add QueryCounter and query_limit_eq

---
+++ lib/rspec/query_limit.rb
@@ -0,0 +1,3 @@
+require 'rspec/query_limit/version'
+require 'rspec/query_limit/query_counter'
+require 'rspec/query_limit/matchers'
---
+++ lib/rspec/query_limit/matchers.rb
@@ -0,0 +1,25 @@
+module Rspec
+  module QueryLimit
+    RSpec::Matchers.define :query_limit_eq do |expected|
+
+      match do |block|
+        query_count(&block) == expected
+      end
+
+      failure_message_for_should do |actual|
+        "Expected to run exactly #{expected} queries, got #{@counter.query_count}"
+      end
+
+      failure_message_for_should_not do |actual|
+        "Expected to run other than #{expected} queries, got #{@counter.query_count}"
+      end
+
+      def query_count(&block)
+        @counter = ActiveRecord::QueryCounter.new
+        ActiveSupport::Notifications.subscribed(@counter.to_proc, 'sql.active_record', &block)
+        @counter.query_count
+      end
+
+    end
+  end
+end
