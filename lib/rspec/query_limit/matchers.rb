e06245d0392cbb5f3490f1ce87554bd4b6487ba9
Jakub Cieslar <cieslar.jakub@gmail.com>
2014-08-08 21:50:09 +0200

    [close #1] RSpec deprecation warnings

--- lib/rspec/query_limit/matchers.rb
+++ lib/rspec/query_limit/matchers.rb
@@ -6,12 +6,22 @@ module Rspec
         query_count(&block) == expected
       end
 
-      failure_message_for_should do |actual|
-        "Expected to run exactly #{expected} queries, got #{@counter.query_count}"
-      end
+      if self.respond_to?(:failure_message)
+        failure_message do |actual|
+          failure_text
+        end
 
-      failure_message_for_should_not do |actual|
-        "Expected to run other than #{expected} queries, got #{@counter.query_count}"
+        failure_message_when_negated  do |actual|
+          failure_text_negated
+        end
+      else
+        failure_message_for_should do |actual|
+          failure_text
+        end
+
+        failure_message_for_should_not do |actual|
+          failure_text_negated
+        end
       end
 
       def query_count(&block)
@@ -24,6 +34,13 @@ module Rspec
         true
       end
 
+      def failure_text
+        "Expected to run exactly #{expected} queries, got #{@counter.query_count}"
+      end
+
+      def failure_text_negated
+        "Expected to run other than #{expected} queries, got #{@counter.query_count}"
+      end
     end
   end
 end
